0d21082857a395cf4d8af52601e5560395f61e1e
Nina Withers <ninarosewithers@gmail.com>
2019-01-05 16:47:11 -0500

    added changes to css

--- assets/js/logic.js
+++ assets/js/logic.js
@@ -1,5 +1,5 @@
 // When the factButton is clicked...
-$("#factButton").on("click", function() {
+$("#factButton").on("click", function () {
 	// We generate a random number between 0 and 4 (the number of facts in the catFactArray)
 	var number = Math.floor((Math.random() * catFactArray.length));
 	// We display the fact from the catFactArray that is in the random position we just generated.
@@ -7,4 +7,4 @@ $("#factButton").on("click", function() {
 })
 
 // This array holds all of our Grumpy Cat facts!
-var catFactArray = ["It is certain.", "It is decidedly so.", "Without a doubt.", "Yes - definitely.", "You may rely on it.", "As I see it, yes.", "Most likely.", "Outlook good.", "Yes.", "Signs point to yes.", "Reply hazy, try again.", "Ask again later.", "Better not tell you now.", "Cannot predict now.", "Concentrate and ask again.", "Don't count on it.", "My reply is no.", "My sources say no.", "Outlook not so good.", "Very doubtful." ]
+var catFactArray = ["It is certain.", "It is decidedly so.", "Without a doubt.", "Yes - definitely.", "You may rely on it.", "As I see it, yes.", "Most likely.", "Outlook good.", "Yes.", "Signs point to yes.", "Reply hazy, try again.", "Ask again later.", "Better not tell you now.", "Cannot predict now.", "Concentrate and ask again.", "Don't count on it.", "My reply is no.", "My sources say no.", "Outlook not so good.", "Very doubtful."]
